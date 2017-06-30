5559c2b4926132bf8267023e4d9f5408a1928e7b
angelali4096 <qingyanl@andrew.cmu.edu>
2017-06-30 11:35:21 -0400

    Modified autogen.sh to auto-generate Makefile.am for manpages
    Added oshcc.1, oshCC.1, oshrun.1 to man folder
    OSHRUN IS NOT COMPLETE

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
 test -d ./config || mkdir ./config
+
+
+printf "dist_man1_MANS = "> ./man/Makefile.am
+find ./man/*.1 -printf "%f " >> ./man/Makefile.am
+printf "\ndist_man3_MANS = ">> ./man/Makefile.am
+find ./man/*.3 -printf "%f " >> ./man/Makefile.am
 
 autoreconf -vif
